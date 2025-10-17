cc7bf88a670a04693b162a728e6351657a44427c
Moh4h <mohammedalmabrook28@gmail.com>
2025-10-17 18:12:37 +0200

    Calculator with font + language + theme

--- lib/component/themes.dart
+++ lib/component/themes.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MyThemes {
-
-
   static ThemeData myLight = ThemeData.light().copyWith(
-    
+    textTheme: GoogleFonts.cairoTextTheme( // 👈 استخدم الخط الذي تريده
+      ThemeData.light().textTheme,
+    ),
+    primaryTextTheme: GoogleFonts.cairoTextTheme(),
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.white,
+      foregroundColor: Colors.black,
+      elevation: 0,
+    ),
   );
 
   static ThemeData myDark = ThemeData.dark().copyWith(
-   
+    textTheme: GoogleFonts.cairoTextTheme( // 👈 نفس الشيء للثيم الداكن
+      ThemeData.dark().textTheme,
+    ),
+    primaryTextTheme: GoogleFonts.cairoTextTheme(),
+    scaffoldBackgroundColor: Colors.black,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.black,
+      foregroundColor: Colors.white,
+      elevation: 0,
+    ),
   );
 }
